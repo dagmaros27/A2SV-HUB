536f4a1d6a3f545ccf6c0154c2dfbce58627bf0b
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-04-15 09:50:07 +0300

    Created 'They Are Everywhere' by A2SV Hub

---
+++ 15-Apr-2024/They Are Everywhere 123395.py
@@ -0,0 +1,29 @@
+# Problem: They Are Everywhere - https://codeforces.com/problemset/problem/701/C
+
+from collections import Counter,defaultdict
+n = int(input())
+w = input()
+
+c = len(Counter(w))
+hmap = defaultdict(int)
+l = 0
+ans = float('inf')
+for r in range(n):
+    hmap[w[r]] += 1
+    
+    while len(hmap) > c:
+        hmap[w[l]] -= 1
+        if hmap[w[l]] == 0:
+            del hmap[w[l]]
+        l += 1
+    while len(hmap) == c:
+        ans = min(ans, r-l+1)
+        hmap[w[l]] -= 1
+        if hmap[w[l]] == 0:
+            del hmap[w[l]]
+        l += 1
+        
+
+print(ans)    
+    
+    
