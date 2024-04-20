11eb462107b72382b595428706c7ad5bcaec5233
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-04-20 11:09:43 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 20-Apr-2024/Petya and Strings 128703.py
@@ -0,0 +1,12 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+word1 = input().lower()
+word2 = input().lower()
+
+
+if word1 < word2:
+    print(-1)
+elif word2 < word1:
+    print(1)
+else:
+    print(0)
