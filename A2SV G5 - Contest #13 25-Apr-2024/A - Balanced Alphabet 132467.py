50f81d2e13a4821c499e64260ab298cb6776ce3e
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-04-25 09:07:39 +0300

    Created 'A - Balanced Alphabet' by A2SV Hub

---
+++ A2SV G5 - Contest #13 25-Apr-2024/A - Balanced Alphabet 132467.py
@@ -0,0 +1,15 @@
+# Problem: A - Balanced Alphabet - https://codeforces.com/gym/519135/problem/A
+
+alpha = "abcdefghijklmnopqrstuvwxyz"
+for _ in range(int(input())):
+    a,b = map(int, input().split())
+    
+    words = list(alpha[:b])
+    ans = []
+    for i in range(a):
+        ans.append(words[i%b])
+    
+    print("".join(ans))
+    
+    
+    
