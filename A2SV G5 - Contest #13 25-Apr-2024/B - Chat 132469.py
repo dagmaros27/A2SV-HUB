13575c763cb951485c5b6d069e0b2f6f88889c21
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-04-25 09:07:43 +0300

    Created 'B - Chat' by A2SV Hub

---
+++ A2SV G5 - Contest #13 25-Apr-2024/B - Chat 132469.py
@@ -0,0 +1,25 @@
+# Problem: B - Chat - https://codeforces.com/gym/519135/problem/B
+
+from heapq import heapify, heappop, heappush, nlargest
+n,k,q = map(int, input().split())
+
+nums = list(map(int, input().split()))
+
+heap = []
+
+for i in range(q):
+    a,b = map(int, input().split())
+    if a == 1:
+        heappush(heap, (nums[b-1], b-1))
+        
+        if len(heap) > k:
+            heappop(heap)
+    else:
+        if (nums[b-1],b-1) in heap:
+            print("YES")
+        else:
+            print("NO")
+
+
+
+
