8dbfdfd22499ce11340814affbe4eaecef307511
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-04-25 09:07:48 +0300

    Created 'C - Poker' by A2SV Hub

---
+++ A2SV G5 - Contest #13 25-Apr-2024/C - Poker 132470.py
@@ -0,0 +1,17 @@
+# Problem: C - Poker - https://codeforces.com/gym/519135/problem/C
+
+from heapq import heappop, heappush
+for _ in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    
+    heap = []
+    ans = 0
+    for i in range(n):
+        if nums[i] != 0:
+            heappush(heap,-nums[i])
+        else:
+            if heap:
+                ans += heappop(heap)
+    print(abs(ans))
+                
