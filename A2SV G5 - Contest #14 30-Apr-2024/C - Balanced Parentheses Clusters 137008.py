24eba1caaa01dcfd21268d83c5e56f8722584e72
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-04-30 16:03:03 +0300

    Created 'C - Balanced Parentheses Clusters' by A2SV Hub

---
+++ A2SV G5 - Contest #14 30-Apr-2024/C - Balanced Parentheses Clusters 137008.py
@@ -0,0 +1,52 @@
+# Problem: C - Balanced Parentheses Clusters - https://codeforces.com/gym/520688/problem/C
+
+for _ in range(int(input())):
+    n = int(input())
+    b = input()
+    
+    hmap = {i:(i,1) for i in range((n)*2)}
+    
+    def find( x):
+        while x != hmap[x][0]:
+            x = hmap[hmap[x][0]][0]
+        
+        return x
+
+
+    def union(x,y):
+        xRep = find(x)
+        yRep = find(y)
+        
+        if xRep != yRep:
+            if hmap[xRep][1] >hmap[yRep][1]:
+                size = hmap[xRep][1] + hmap[yRep][1]
+                hmap[yRep] = [xRep,size]
+                hmap[xRep] = [xRep,size]
+            else:
+                size = hmap[xRep][1] + hmap[yRep][1]
+                hmap[yRep] = [yRep,size]
+                hmap[xRep] = [yRep,size]
+    
+    opens = []
+    for i in range(n*2):
+        if b[i] == "(":
+            opens.append(i)
+        if b[i] == ")":
+            v = opens.pop()
+            union(i,v)
+            if i +1  < 2*n and b[i+1]=="(":
+                union(i,i+1)
+
+    
+    ans = 0
+    for key in hmap:
+        if key == hmap[key][0]:
+            ans += 1
+    print(ans)
+                
+    
+    
+
+            
+            
+            
