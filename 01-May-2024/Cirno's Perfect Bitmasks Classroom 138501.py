e07f8c8b1b220e8f2a855606d6047ebe3add7f04
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-01 19:33:45 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 138501.py
@@ -0,0 +1,23 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+for _ in range(int(input())):
+    n = int(input())
+    ans = 1
+    ns = bin(n)
+    i = -1
+    if n==1: 
+        print(3) 
+        continue
+    while i > -(len(ns)-2) and ns[i] != "1":
+        ans = ans<<1
+        i -= 1
+    if i == -(len(ns)-2):
+        m = 1
+        while n & m:
+            n = n & (~m)
+            m = m << 1
+        n = n | m
+        print(n)
+    else:
+        print(ans)
+            
