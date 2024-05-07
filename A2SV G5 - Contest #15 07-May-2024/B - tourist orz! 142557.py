f08a0fc13c12305487bdfbde517d10f1872e971e
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-07 18:04:21 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/B - tourist orz! 142557.py
@@ -0,0 +1,15 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+for _ in range(int(input())):
+    a,b = map(int, input().split())
+    nums = list(map(int, input().split()))
+    
+    mxm = b
+    
+    for n in nums:
+        mxm = max(mxm, b|n,n)
+    
+    print(mxm)
+    
+    
+    
