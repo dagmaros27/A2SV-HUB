04ba7f8949f03dfca0bd96071d5f4d4e8deeee80
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-07 18:04:28 +0300

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/A - a-e-i-o-u-y 142558.py
@@ -0,0 +1,15 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+n = int(input())
+w = input()
+vowels = set(['a','e','i','o','u','y'])
+prevv = False
+
+i=1
+while i < len(w):
+    if w[i-1] in vowels and w[i] in vowels:
+        w = w[:i] + w[i+1:]
+        i -= 1
+    i += 1
+
+print(w) 
