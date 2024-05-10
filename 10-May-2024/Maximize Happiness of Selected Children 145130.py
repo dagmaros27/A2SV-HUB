7d07599e2c0ccc512911c721137dfe98ee39c40c
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-10 08:33:51 +0300

    Created 'Maximize Happiness of Selected Children' by A2SV Hub

---
+++ 10-May-2024/Maximize Happiness of Selected Children 145130.py
@@ -0,0 +1,17 @@
+# Problem: Maximize Happiness of Selected Children - https://leetcode.com/problems/maximize-happiness-of-selected-children/?envType=daily-question&envId=2024-05-09
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort()
+        
+        turn = 0
+        ans = 0
+        for i in range(len(happiness)-1,len(happiness)-k-1,-1):
+            if happiness[i] - turn >0:
+                ans += (happiness[i] - turn)
+            turn += 1
+            
+        return ans
+
+
+        
