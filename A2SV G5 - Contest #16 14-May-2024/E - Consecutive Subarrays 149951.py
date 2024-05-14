46cddc86f6ac8a72670eccd1d1cf23b52f7f2894
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-14 18:39:55 +0300

    Created 'E - Consecutive Subarrays' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/E - Consecutive Subarrays 149951.py
@@ -0,0 +1,20 @@
+# Problem: E - Consecutive Subarrays - https://codeforces.com/gym/523525/problem/E
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+sum = 0
+arr = []
+for i in range(n - 1, -1, -1):
+    sum += a[i]
+    if i > 0:
+        arr.append(sum)
+
+res = sum
+
+arr.sort(reverse=True)
+
+for i in range(k - 1):
+    res += arr[i]
+
+print(res)
