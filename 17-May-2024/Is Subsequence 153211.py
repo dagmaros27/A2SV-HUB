9063be3cc911e225f238e2a4a3715653dbfec400
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-17 18:24:51 +0300

    Created 'Is Subsequence' by A2SV Hub

---
+++ 17-May-2024/Is Subsequence 153211.py
@@ -0,0 +1,16 @@
+# Problem: Is Subsequence - https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        p1,p2 = 0,0
+
+        while p1 < len(s) and p2 < len(t):
+            if s[p1] == t[p2]:
+                p1 += 1
+            p2 += 1
+        
+        if p1 == len(s):
+            return True
+        return False
+
+        
