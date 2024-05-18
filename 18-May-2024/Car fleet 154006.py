f5885dfd5648c2827f742dc666318b71d692d4d5
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-05-18 20:45:24 +0300

    Created 'Car fleet' by A2SV Hub

---
+++ 18-May-2024/Car fleet 154006.py
@@ -0,0 +1,19 @@
+# Problem: Car fleet - https://leetcode.com/problems/car-fleet/
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        pos_speed = [(pos, speed) for pos, speed in zip(position, speed)]
+        pos_speed.sort(reverse=True, key=lambda x: x[0])
+        
+        stack = []
+        
+        for pos, speed in pos_speed:
+            
+            time = (target - pos) / speed
+            
+            if stack and stack[-1] >= time:
+                time = stack.pop()
+            stack.append(time)
+        
+        return len(stack)
