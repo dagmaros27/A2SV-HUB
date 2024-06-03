ddadfb99c88b40013128f138bca1b74be4ac9ed2
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-06-03 21:50:33 +0300

    Created '3Sum' by A2SV Hub

---
+++ 03-Jun-2024/3Sum 163622.py
@@ -0,0 +1,24 @@
+# Problem: 3Sum - https://leetcode.com/problems/3sum/description/
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        nums.sort()
+        
+        for  i in range(len(nums)-2):
+            if i> 0 and nums[i] == nums[i-1]:
+                continue
+            j , k = i+1,len(nums)-1
+            
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == 0:
+                    ans.add((nums[i],nums[j],nums[k]))
+                    j += 1
+                    k -=1
+                elif sum > 0:
+                    k -= 1
+                else:
+                    j += 1
+        
+        return list(ans)
