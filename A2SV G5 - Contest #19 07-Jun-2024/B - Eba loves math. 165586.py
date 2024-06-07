da0c640ab8bb8a934e4c2189adb4060a1f1f5b25
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-06-07 22:18:09 +0300

    Created 'B - Eba loves math.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 07-Jun-2024/B - Eba loves math. 165586.py
@@ -0,0 +1,12 @@
+# Problem: B - Eba loves math. - https://codeforces.com/gym/527294/problem/B
+
+for _ in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    
+    ans = nums[0]
+    
+    for num in nums:
+        ans &= num
+    
+    print(ans)  
