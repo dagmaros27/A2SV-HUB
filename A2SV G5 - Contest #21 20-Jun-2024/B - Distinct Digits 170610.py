2dc491ddb06a5aa1a08e358b13f0fd3cdaf2fadd
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-06-20 14:25:00 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/B - Distinct Digits 170610.py
@@ -0,0 +1,37 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+import sys
+import threading
+from sys import stdin, stdout, setrecursionlimit
+from collections import defaultdict
+
+input = lambda: sys.stdin.readline().strip()
+
+setrecursionlimit(1 << 30)
+threading.stack_size(1 << 27)
+
+def main():
+    for _ in range(int(input())):
+        n = int(input())
+        ans = float('inf')
+        
+        def gen(sum, num, start):
+            nonlocal ans
+            if sum == n:
+                ans = min(ans, int(num))
+                return
+            if sum > n:
+                return
+            
+            for digit in range(start, 10):
+                gen(sum + digit, num + str(digit), digit + 1)
+        
+        gen(0, "", 1)
+        print(ans)
+
+
+
+main_thread = threading.Thread(target=main)
+main_thread.start()
+main_thread.join()
+
