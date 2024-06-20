29ac1b3e80298f42dbbea742e6a5c13a6e071ed9
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-06-20 14:24:55 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/A - abbccc 170609.py
@@ -0,0 +1,14 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+n = int(input())
+w = input()
+ans = [w[0]]
+diff = 1
+i = 0
+while i + diff < n:
+    i += diff
+    ans.append(w[i])
+    diff += 1
+    
+print("".join(ans))
+    
