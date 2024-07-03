bd903c04900e56b83d164448287c3e3e4051cc39
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-07-03 19:20:10 +0300

    Created 'Minimum Difference Between Largest and Smallest Value in 3 Moves' by A2SV Hub

---
+++ 03-Jul-2024/Minimum Difference Between Largest and Smallest Value in 3 Moves 175995.py
@@ -0,0 +1,13 @@
+# Problem: Minimum Difference Between Largest and Smallest Value in 3 Moves - https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        
+        if len(nums) <= 4:
+            return 0
+        
+        minDifference = min( [nums[-1] - nums[3], nums[-2] - nums[2], nums[-3] - nums[1], nums[-4] - nums[0]] )
+        
+        return minDifference
+            
