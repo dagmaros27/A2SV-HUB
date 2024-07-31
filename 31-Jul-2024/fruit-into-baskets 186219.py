95c344b1f4125672b0a53e8c89017873ddb2bc0a
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-07-31 14:29:50 +0300

    Created 'fruit-into-baskets' by A2SV Hub

---
+++ 31-Jul-2024/fruit-into-baskets 186219.py
@@ -0,0 +1,20 @@
+# Problem: fruit-into-baskets - https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        hmap = defaultdict(int)
+        max_num = 0
+
+        l = 0
+
+        for r in range(len(fruits)):
+            hmap[fruits[r]] += 1
+            
+            while len(hmap) > 2:
+                hmap[fruits[l]] -= 1
+                if hmap[fruits[l]] == 0:
+                    del hmap[fruits[l]]
+                l += 1
+            max_num = max(max_num, sum(hmap.values()))
+        
+        return max_num
