db38127b4a29674c2dc4e048cc0088cb2770aea5
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-08-10 21:56:55 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 10-Aug-2024/A - Ascend 190363.py
@@ -0,0 +1,16 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+c = 1
+mxm = 1
+for i in range(1,n):
+    if nums[i] > nums[i-1]:
+        c += 1
+    else:
+        c = 1
+    mxm = max(mxm,c)
+        
+    
+print(mxm)
