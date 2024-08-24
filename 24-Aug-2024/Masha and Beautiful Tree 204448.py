9414bce855b8ae6b7f44b406d12e29b2404fbd4f
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-08-24 11:46:53 +0300

    Created 'Masha and Beautiful Tree' by A2SV Hub

---
+++ 24-Aug-2024/Masha and Beautiful Tree 204448.py
@@ -0,0 +1,30 @@
+# Problem: Masha and Beautiful Tree - https://codeforces.com/problemset/problem/1741/D
+
+def merge(left, right):
+    count = 0  
+    if left[0] > right[0]:
+        count += 1  
+        return left + right, count
+    return right + left, count
+ 
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr, 0  
+ 
+    m = len(arr) // 2
+    left, left_count = mergeSort(arr[:m])
+    right, right_count = mergeSort(arr[m:])
+ 
+    merged, count = merge(left, right)
+    return merged, count + left_count + right_count
+ 
+for _ in range(int(input())):
+    n = int(input())
+    x = list(map(int, input().split()))
+    sorted_x,count = mergeSort(x)
+    ans = list(reversed(sorted_x))
+ 
+    if ans == sorted(x):
+        print(count)
+    else:
+        print(-1
