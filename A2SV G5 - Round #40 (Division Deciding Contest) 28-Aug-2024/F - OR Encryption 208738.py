383c3be56b3ef301335a21a3969b58e94fe588bc
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-08-28 22:02:38 +0300

    Created 'F - OR Encryption' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/F - OR Encryption 208738.py
@@ -0,0 +1,32 @@
+# Problem: F - OR Encryption - https://codeforces.com/gym/543431/problem/F
+
+for _ in range(int(input())):
+    n = int(input())
+    mat = []
+    for _ in range(n):
+        nums = list(map(int, input().split()))
+        mat.append(nums)
+      
+    a = [2**30 - 1] * n
+    
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                a[i] &= mat[i][j]
+                a[j] &= mat[i][j]
+
+    flag = False
+    for i in range(n):
+        for j in range(n):
+            if i != j and a[i] | a[j] != mat[i][j]:
+                print("NO")               
+                flag = True
+            if flag:
+                break
+        if flag:
+            break
+    if flag:
+        continue
+            
+    print("YES")
+    print(*a)
