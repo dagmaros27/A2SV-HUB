638501585357ec0c6346310eb1aa41d8281bc04d
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-09-03 23:02:03 +0300

    Created 'Meaningless Operations' by A2SV Hub

---
+++ 03-Sep-2024/Meaningless Operations 210813.py
@@ -0,0 +1,17 @@
+# Problem: Meaningless Operations - https://codeforces.com/problemset/problem/1110/C
+
+from math import gcd, log2, ceil
+
+for _ in range(int(input())):
+    n = int(input())
+
+    mxm_val = (1 << int(ceil(log2(n + 1)))) - 1 
+    if n == mxm_val:
+        ans = 1
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                ans = max(ans, i, n // i)
+        print(ans)
+    else:
+        print(mxm_val)
+
