0405c41d31c820805fb463eedf849289f3ac6b16
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-09-11 21:46:04 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 11-Sep-2024/Replace Words 214391.py
@@ -0,0 +1,33 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        
+        def find(roots, word):
+            for root in roots:
+                if word.startswith(root):
+                    return root
+            
+            return None
+
+        dic = defaultdict(list)
+
+        for d in dictionary:
+            dic[d[0]].append(d)
+        
+        for arr in dic:
+            dic[arr].sort(key = lambda x: len(x))
+        
+        words = sentence.split()
+
+        ans = []
+        for word in words:
+            root = find(dic[word[0]], word)
+            if root is not None:
+                ans.append(root)
+            else:
+                ans.append(word)
+        
+        return " ".join(ans)
+
+        
