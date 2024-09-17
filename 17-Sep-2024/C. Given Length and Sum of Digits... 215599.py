0047e2104f95c1a4ab1105e27ea4690e400e285a
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2024-09-17 16:03:25 +0300

    Created 'C. Given Length and Sum of Digits...' by A2SV Hub

---
+++ 17-Sep-2024/C. Given Length and Sum of Digits... 215599.py
@@ -0,0 +1,40 @@
+# Problem: C. Given Length and Sum of Digits... - https://codeforces.com/contest/489/problem/C
+
+m, s = map(int, input().split())
+if s == 0 :
+    if m == 1:
+        print(0,0)
+        exit()
+    print(-1, -1)
+    exit()
+
+mxm = []
+ 
+for i in range(m):
+    if s > 9:
+        mxm.append(9)
+        s -= 9
+    else:
+        mxm.append(s)
+        s -= s
+    
+mnm = list(reversed(mxm))
+ 
+nonzero = 0
+ 
+while mnm[nonzero] == 0:
+    
+    nonzero += 1
+ 
+mnm[0] += 1
+mnm[nonzero] -= 1
+ 
+if s == 0:
+                
+    mnm = map(str, mnm)
+    mxm = map(str, mxm)
+ 
+ 
+    print("".join(mnm), "".join(mxm))
+else:
+    print(-1, -1)
